c3869172a260fc17d8398151f0f5f13e35348abd
Rahul Nag <rahulnag625@gmail.com>
2023-06-30 18:12:41 +0530

    about og image corrected

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -5,6 +5,28 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "About",
   description: "About Rahul Nag",
+  openGraph: {
+    title: "About Rahul Nag",
+    description: "A highly experienced web developer with 6 year of experience",
+    url: "https://www.rahulnag.in/about",
+    siteName: "Rahul Nag",
+    images: [
+      {
+        url: "https://rahulnag.in/profile.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://rahulnag.in/profile.png",
+        width: 1800,
+        height: 1600,
+        alt: "Rahul Nag",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+    authors: ["Rahul Nag"],
+  },
 };
 
 export default function Layout({ children }) {
