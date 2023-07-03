97b0ec08a97943f249798e4c844bb3ebde6330b3
Rahul Nag <rahulnag625@gmail.com>
2023-07-03 13:10:30 +0530

    testing seo 1

---
+++ src/app/contact/mycontact/page.js
@@ -0,0 +1,41 @@
+import React from "react";
+import webcontent from "@/webcontent.json";
+import styles from "./page.module.css";
+import SocialMediaLink from "@/components/SocialMediaLink";
+
+// export const metadata = {
+//   title: "Contact",
+//   description: "Contact Rahul Nag",
+//   openGraph: {
+//     title: "Contact Rahul Nag",
+//     description: "A highly experienced web developer with 6 year of experience",
+//     url: "https://www.rahulnag.in/contact",
+//     siteName: "Contact Rahul Nag",
+//     images: [
+//       {
+//         url: "https://rahulnag.in/profile.png",
+//         width: 800,
+//         height: 600,
+//       },
+//       {
+//         url: "https://rahulnag.in/profile.png",
+//         width: 1800,
+//         height: 1600,
+//         alt: "Rahul Nag",
+//       },
+//     ],
+//     locale: "en_US",
+//     type: "website",
+//     authors: ["Rahul Nag"],
+//   },
+// };
+
+const page = () => {
+  return (
+    <div className={styles.contactHolder}>
+      <h1>HI........</h1>
+    </div>
+  );
+};
+
+export default page;
